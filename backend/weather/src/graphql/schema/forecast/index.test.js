40ce338eedbd828a422ecd99f38c4c66682751d5
danwakeem <danwakeem@gmail.com>
2021-07-11 08:11:57 -0500

    Updated backend tests

--- backend/weather/src/graphql/schema/forecast/index.test.js
+++ backend/weather/src/graphql/schema/forecast/index.test.js
@@ -172,6 +172,167 @@ describe('GQL::Forecast', () => {
       });
     });
 
+    it('should return forecast with units set to metric', async () => {
+      const hourlyMock = {
+        data: {
+          city: {
+            name: 'Test',
+            country: 'US',
+          },
+          list: [
+            {
+              dt: 1625859789,
+              main: {
+                temp: 80,
+                feels_like: 90,
+              },
+              weather: [
+                {
+                  icon: '1b',
+                  description: 'hi mom',
+                },
+              ],
+            },
+            {
+              dt: 1625427851,
+              main: {
+                temp: 70,
+                feels_like: 100,
+              },
+              weather: [
+                {
+                  icon: '1c',
+                  description: 'hi dad',
+                },
+              ],
+            },
+          ],
+        },
+      };
+
+      const dailyMock = {
+        data: {
+          timezone_offset: -1800,
+          daily: [
+            {
+              dt: 1625859797,
+              temp: {
+                min: 1,
+                max: 100,
+              },
+              uvi: 0.98,
+              weather: [
+                {
+                  description: 'hi brother',
+                },
+              ],
+            },
+            {
+              dt: 1625427873,
+              temp: {
+                min: 4,
+                max: 10,
+              },
+              uvi: 0.99,
+              weather: [
+                {
+                  description: 'hi sister',
+                },
+              ],
+            },
+          ],
+        },
+      };
+
+      axios.get.mockResolvedValueOnce(hourlyMock);
+      axios.get.mockResolvedValueOnce(dailyMock);
+
+      const expectedZipCode = '75070';
+      const expectedUnits = 'metric';
+      const out = await testServer.query({
+        query: gql`
+          query GET_FORECAST($zip: String!, $units: Units) {
+            forecast(zip: $zip, units: $units) {
+              id
+              city
+              country
+              daily {
+                id
+                dt
+                min
+                max
+                uvi
+                description
+                hourly {
+                  id
+                  dt
+                  temp
+                  feelsLike
+                  icon
+                  description
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          zip: expectedZipCode,
+          units: expectedUnits,
+        },
+      });
+
+      expect(axios.get).toHaveBeenCalledWith(expect.any(String), {
+        params: {
+          zip: expectedZipCode,
+          appId: process.env.API_KEY,
+          units: expectedUnits
+        },
+      });
+      expect(out.data.forecast).toEqual({
+        id: expectedZipCode,
+        city: hourlyMock.data.city.name,
+        country: hourlyMock.data.city.country,
+        daily: [
+          {
+            id: `75070:${dailyMock.data.daily[0].dt.toString()}`,
+            dt: dailyMock.data.daily[0].dt,
+            min: dailyMock.data.daily[0].temp.min,
+            max: dailyMock.data.daily[0].temp.max,
+            uvi: dailyMock.data.daily[0].uvi,
+            description: dailyMock.data.daily[0].weather[0].description,
+            hourly: [
+              {
+                id: `75070:${hourlyMock.data.list[0].dt.toString()}`,
+                dt: hourlyMock.data.list[0].dt,
+                temp: hourlyMock.data.list[0].main.temp,
+                feelsLike: hourlyMock.data.list[0].main.feels_like,
+                icon: hourlyMock.data.list[0].weather[0].icon,
+                description: hourlyMock.data.list[0].weather[0].description,
+              },
+            ],
+          },
+          {
+            id: `75070:${dailyMock.data.daily[1].dt.toString()}`,
+            dt: dailyMock.data.daily[1].dt,
+            min: dailyMock.data.daily[1].temp.min,
+            max: dailyMock.data.daily[1].temp.max,
+            uvi: dailyMock.data.daily[1].uvi,
+            description: dailyMock.data.daily[1].weather[0].description,
+            hourly: [
+              {
+                id: `75070:${hourlyMock.data.list[1].dt.toString()}`,
+                dt: hourlyMock.data.list[1].dt,
+                temp: hourlyMock.data.list[1].main.temp,
+                feelsLike: hourlyMock.data.list[1].main.feels_like,
+                icon: hourlyMock.data.list[1].weather[0].icon,
+                description: hourlyMock.data.list[1].weather[0].description,
+              },
+            ],
+          },
+        ],
+      });
+    });
+
     it('should return error if zip is not found in map', async () => {
       const hourlyMock = {
         data: {
